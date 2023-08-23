d7acac37a236c11819370f6b16e1ba145cdcd1bd
luis <linkinluisave@gmail.com>
2023-08-23 14:57:58 -0600

    added snackbar

--- app/src/main/java/com/linkinaplications/jetpackcomposecomponents/MainActivity.kt
+++ app/src/main/java/com/linkinaplications/jetpackcomposecomponents/MainActivity.kt
@@ -14,10 +14,13 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.linkinaplications.jetpackcomposecomponents.ui.theme.JetpackComposeComponentsTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,26 +40,39 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ScaffoldExample() {
-    Scaffold(topBar = { MyTopAppBar() }){
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        topBar = {
+            MyTopAppBar {
+                coroutineScope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar(
+                        "has puldado $it"
+                    )
+                }
+            }
+        }, scaffoldState = scaffoldState
+    ) {
 
     }
 }
 
 @Composable
-fun MyTopAppBar() {
+fun MyTopAppBar(onClickIcon: (String) -> Unit) {
     TopAppBar(
         title = { Text(text = "Mi primera toolbar", color = Color.White) },
         backgroundColor = Color.Red,
         navigationIcon = {
-            IconButton(onClick = { }) {
+            IconButton(onClick = { onClickIcon("Atrás") }) {
                 Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "back")
             }
         },
         actions = {
-            IconButton(onClick = { }) {
+            IconButton(onClick = { onClickIcon("Buscar") }) {
                 Icon(imageVector = Icons.Filled.Search, contentDescription = "search")
             }
-            IconButton(onClick = { }) {
+            IconButton(onClick = { onClickIcon("Exit") }) {
                 Icon(imageVector = Icons.Filled.ExitToApp, contentDescription = "exist")
             }
         }
